5ce288b12be7a6c9386b7314e6038c04c520ac6a
Denis Obydennykh <denis.obydennykh@gmail.com>
2019-01-29 00:55:59 -0500

    example schematics

--- src/my-full-schematic/index.ts
+++ src/my-full-schematic/index.ts
@@ -1,31 +1,15 @@
 import {
   Rule,
   SchematicContext,
-  Tree,
-  apply,
-  chain,
-  mergeWith,
-  schematic,
-  template,
-  url,
+  Tree
 } from '@angular-devkit/schematics';
 
 
 // todo deniso Instead of `any`, it would make sense here to get a schema-to-dts package and output the
 // interfaces so you get type-safe options.
 export default function (options: any): Rule {
-  return chain([
-    (_tree: Tree, context: SchematicContext) => {
-      context.logger.info('My Full Schematic: ' + JSON.stringify(options));
-    },
-
-    schematic('my-other-schematic', { option: true }),
-
-    mergeWith(apply(url('./files'), [
-      template({
-        INDEX: options.index,
-        name: options.name,
-      }),
-    ])),
-  ]);
+  return (_tree: Tree, context: SchematicContext) => {
+    options;
+    context.logger.info('My Full Schematic: ' + JSON.stringify(_tree.actions));
+  }
 }
--- src/my-schematic/index.ts
+++ src/my-schematic/index.ts
@@ -1,11 +1,46 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {chain, Rule, schematic, SchematicContext, Tree} from '@angular-devkit/schematics';
+
+const createFile: Rule = (tree: Tree, context: SchematicContext) => {
+  context.logger.info('File is created');
+  tree.create('hello.ts', 'class ClassWithOutFormatting{ public x = 1; y = function(){return 2}}');
+  return tree;
+};
+
+const overwriteFile: Rule = (tree: Tree, context: SchematicContext) => {
+  context.logger.info('File is overwritten');
+  let path = 'src/main.ts';
+  const content: Buffer | null = tree.read(path);
+  const newContent = content!.toString().replace('enableProdMode();', `
+    // enableProdMode here
+    enableProdMode();
+  `);
+  debugger;
+  tree.overwrite(path, newContent);
+  return tree;
+};
+
+const modifyFile: Rule = (tree: Tree, context: SchematicContext) => {
+  context.logger.info('File is modified');
+  let path = 'src/app/app.component.ts';
+  const content: Buffer | null = tree.read(path);
+  let stringToSearch = 'AppComponent';
+  const index = content!.indexOf(stringToSearch);
+
+  const updateRecorder = tree.beginUpdate(path);
+  updateRecorder.insertRight(index + stringToSearch.length, ` /* here may be 'extends BlaBlaBla' */ `);
+  tree.commitUpdate(updateRecorder);
+
+  return tree;
+};
 
 
-export function mySchematic(options: any): Rule {
-  return (tree: Tree, context: SchematicContext) => {
-    options;
-    context;
-    tree.create('hello.ts', 'class ClassWithOutFormatting{ public x = 1; y = function(){return 2}}');
-    return tree;
-  };
+export default function mySchematic(options: any): Rule {
+  options;
+  const rules = [createFile,
+  overwriteFile];
+  rules;
+  return chain([
+    modifyFile,
+    schematic('my-full-schematic', {name: 'hack off'}),
+  ]);
 }
