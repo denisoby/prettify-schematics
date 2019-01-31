eef67621410cb16e7f79b49aefacede8a4860c8f
Denis Obydennykh <denis.obydennykh@gmail.com>
2019-01-31 02:19:56 -0500

    get diff from actions, next step - run prettier for dif changes

--- src/my-full-schematic/index.ts
+++ src/my-full-schematic/index.ts
@@ -1,15 +1,87 @@
 import {
+  Action,
   Rule,
   SchematicContext,
   Tree
-} from '@angular-devkit/schematics';
+} from "@angular-devkit/schematics";
 
+import * as fs from "fs";
+import {Hunk, structuredPatch} from "diff";
+
+interface ChangedFileAndLines {
+  path: string;
+  lines?: {
+    start: number; // 0 based
+    finish: number;
+  };
+}
 
 // todo deniso Instead of `any`, it would make sense here to get a schema-to-dts package and output the
 // interfaces so you get type-safe options.
-export default function (options: any): Rule {
+export default function(options: any): Rule {
   return (_tree: Tree, context: SchematicContext) => {
     options;
-    context.logger.info('My Full Schematic: ' + JSON.stringify(_tree.actions));
-  }
+
+    let actions = _tree.actions;
+    const changedFiles: ChangedFileAndLines[] = [];
+    actions.forEach((action: Action) => {
+      if (!(action as any).content) {
+        return;
+      }
+
+      const path = action.path;
+      const fileChanged = {
+        path
+      };
+
+      changedFiles.push(fileChanged);
+
+      if (action.kind === "c") {
+        // file is created and all lines are changed
+        return;
+      }
+
+      // let's create diff and compare to find changes
+
+      const newContent = (action as any).content.toString();
+      const fullPath = process.cwd() + path;
+
+      console.log("fullPath: ", fullPath);
+      const oldContent = fs.readFileSync(fullPath, {encoding: 'utf-8'});
+
+      console.log("oldContent: ", oldContent);
+
+      const diff = structuredPatch(
+        "oldFileName",
+        "newFileName",
+          newContent,
+          oldContent,
+        "oldHeader",
+        "newHeader",
+        {
+          newlineIsToken: true
+        }
+      );
+
+      // const FgRed = "\x1b[31m";
+      // const FgGreen = "\x1b[32m";
+      // const FgWhite = "\x1b[37m";
+
+      diff.hunks.forEach((part: Hunk) => {
+        // green for additions, red for deletions
+        // grey for common parts
+        console.log('---------- next hunk');
+        console.log(JSON.stringify(part, null, 4));
+      });
+    });
+
+    let actionsForLogging = actions.map((action: Action) => ({
+      ...action,
+      content: "--content--"
+    }));
+
+    context.logger.info(
+      "My Full Schematic: " + JSON.stringify(actionsForLogging, null, 4)
+    );
+  };
 }
--- src/my-schematic/index.ts
+++ src/my-schematic/index.ts
@@ -1,46 +1,60 @@
-import {chain, Rule, schematic, SchematicContext, Tree} from '@angular-devkit/schematics';
+import {
+  chain,
+  Rule,
+  schematic,
+  SchematicContext,
+  Tree
+} from "@angular-devkit/schematics";
 
 const createFile: Rule = (tree: Tree, context: SchematicContext) => {
-  context.logger.info('File is created');
-  tree.create('hello.ts', 'class ClassWithOutFormatting{ public x = 1; y = function(){return 2}}');
+  context.logger.info("File is created");
+  tree.create(
+    "hello.ts",
+    "class ClassWithOutFormatting{ public x = 1; y = function(){return 2}}"
+  );
   return tree;
 };
 
 const overwriteFile: Rule = (tree: Tree, context: SchematicContext) => {
-  context.logger.info('File is overwritten');
-  let path = 'src/main.ts';
+  context.logger.info("File is overwritten");
+  let path = "src/main.ts";
   const content: Buffer | null = tree.read(path);
-  const newContent = content!.toString().replace('enableProdMode();', `
+  const newContent = content!.toString().replace(
+    "enableProdMode();",
+    `
     // enableProdMode here
     enableProdMode();
-  `);
+  `
+  );
   debugger;
   tree.overwrite(path, newContent);
   return tree;
 };
 
 const modifyFile: Rule = (tree: Tree, context: SchematicContext) => {
-  context.logger.info('File is modified');
-  let path = 'src/app/app.component.ts';
+  context.logger.info("File is modified");
+  let path = "src/app/app.component.ts";
   const content: Buffer | null = tree.read(path);
-  let stringToSearch = 'AppComponent';
+  let stringToSearch = "AppComponent";
   const index = content!.indexOf(stringToSearch);
 
   const updateRecorder = tree.beginUpdate(path);
-  updateRecorder.insertRight(index + stringToSearch.length, ` /* here may be 'extends BlaBlaBla' */ `);
+  updateRecorder.insertRight(
+    index + stringToSearch.length,
+    ` /* here may be 'extends BlaBlaBla' */ `
+  );
   tree.commitUpdate(updateRecorder);
 
   return tree;
 };
 
-
 export default function mySchematic(options: any): Rule {
   options;
-  const rules = [createFile,
-  overwriteFile];
+  const rules = [createFile, overwriteFile];
   rules;
   return chain([
+    ...rules,
     modifyFile,
-    schematic('my-full-schematic', {name: 'hack off'}),
+    schematic("my-full-schematic", { name: "hack off" })
   ]);
 }
